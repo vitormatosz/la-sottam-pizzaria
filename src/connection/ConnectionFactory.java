b9d908fc968908398275865e88c3a722a96b9410
samiratoledo <samiracarvalhotoledo@gmail.com>
2026-09-08 21:13:04 -0300

    adição da pag clientes com crud, add da biblioteca sql,ajuste no login para rodar em primeira tela o clientes

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private static final String URL = "jdbc:mysql://localhost:3307/lasottam";
+    private static final String URL = "jdbc:mysql://localhost:3306/lasottam";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
